bf9d64fbfcd4f85db34b2e7b85dd2bfae063a419
Augusto Pin <augustopm607@gmail.com>
2025-06-03 09:16:35 -0500

    feat: add post of new bond

--- InverBono/bonds/views.py
+++ InverBono/bonds/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, redirect
 from .models import Bond
+from decimal import Decimal, InvalidOperation
 
 def create_bond_view(request):
     if request.method == 'POST':
+        def parse_decimal(val):
+            try:
+                return Decimal(val) if val not in (None, '', ' ') else None
+            except (InvalidOperation, TypeError):
+                return None
+
         bond_data = {
-            'nominal_value': request.POST.get('nominal_value'),
-            'commercial_value': request.POST.get('commercial_value'),
+            'nominal_value': parse_decimal(request.POST.get('nominal_value')),
+            'commercial_value': parse_decimal(request.POST.get('commercial_value')),
             'issue_date': request.POST.get('issue_date'),
-            'years_number': request.POST.get('years_number'),
+            'years_number': parse_decimal(request.POST.get('years_number')),
             'coupon_frequency': request.POST.get('coupon_frequency'),
             'interest_rate_type': request.POST.get('interest_rate_type'),
             'capitalization': request.POST.get('capitalization'),
-            'interest_rate': request.POST.get('interest_rate'),
-            'annual_discount_rate': request.POST.get('annual_discount_rate'),
-            'income_tax': request.POST.get('income_tax'),
-            'premium_percentage': request.POST.get('premium_percentage'),
-            'structuring_percentage': request.POST.get('structuring_percentage'),
-            'placement_percentage': request.POST.get('placement_percentage'),
-            'float_percentage': request.POST.get('float_percentage'),
-            'cavali_percentage': request.POST.get('cavali_percentage'),
+            'interest_rate': parse_decimal(request.POST.get('interest_rate')),
+            'annual_discount_rate': parse_decimal(request.POST.get('annual_discount_rate')),
+            'income_tax': parse_decimal(request.POST.get('income_tax')),
+            'premium_percentage': parse_decimal(request.POST.get('premium_percentage')),
+            'structuring_percentage': parse_decimal(request.POST.get('structuring_percentage')),
+            'placement_percentage': parse_decimal(request.POST.get('placement_percentage')),
+            'float_percentage': parse_decimal(request.POST.get('float_percentage')),
+            'cavali_percentage': parse_decimal(request.POST.get('cavali_percentage')),
         }
-
-        Bond.objects.create(**bond_data)
-
-        # return redirect('success_page')
-
+        bond_data = {k: v for k, v in bond_data.items() if v is not None or k in ['nominal_value', 'commercial_value', 'issue_date', 'years_number', 'coupon_frequency', 'interest_rate_type', 'capitalization', 'interest_rate', 'annual_discount_rate', 'income_tax']}
+        try:
+            Bond.objects.create(**bond_data)
+            return redirect('dashboard')
+        except Exception as e:
+            return render(request, 'create-bond.html', {'error': str(e)})
     return render(request, 'create-bond.html')
